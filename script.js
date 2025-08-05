5ac23c0a042a08b42d1f8f2fc001cb6d3574ed5b
Raulwi3B <133982365+Raulwi1b@users.noreply.github.com>
2025-08-05 12:38:30 +0000

    oi

---
+++ script.js
@@ -0,0 +1,53 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Chegando em casa voce verifica o conteúdo visto e o analisa sériamente, observando que está com dificuldade você usaria a IA para ajudá-lo??",
+        alternativas: [
+            "Não! bicho burro não sabe nem adição",
+            "sim!"
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora da escola decidiu fazer um trabalho de matemática de um assunto de extrema dificuldade (binômios de Newton completo), o que você ",
+        alternativas: [
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Faz algumas pesquisas na internet e assiste vídeo-aulas sobre o assunto.",
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta a educção no futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            "Defende a ideia de que a IA pode criar novas e melhores novam formas de gestão educacional e melhorar o raciocíonio humano.",
+            "Me preocupo com o futuro educacional tangente à educação futura."
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "Criar uma imagem utilizando um gerador de imagem de IA."
+        ]
+    },
+    {
+        enunciado: "Você precisa entregar o trab para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        alternativas: [
+           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
