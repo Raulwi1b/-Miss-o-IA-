df235dacea61a85847b918310691874989b67dd0
Raulwi3B <133982365+Raulwi1b@users.noreply.github.com>
2025-08-19 13:28:02 +0000

    ok

--- script.js
+++ script.js
@@ -6,48 +6,107 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Chegando em casa voce verifica o conteúdo visto e o analisa sériamente, observando que está com dificuldade você usaria a IA para ajudá-lo??",
+        enunciado: "Você acabou de sair a escola, e foi encurralao por um ladrão armado. Ele te pergunta quanto val o PI",
         alternativas: [
-            "Não! bicho burro não sabe nem adição",
-            "sim!"
+            {
+                texto: "é aproxomadamnte 3,14",
+                afirmacao: "ele lhe parabeniza"
+            },
+            {
+                texto: " não sei....",
+                afirmacao: "o ladrão rouba sua mochila"
+            }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora da escola decidiu fazer um trabalho de matemática de um assunto de extrema dificuldade (binômios de Newton completo), o que você ",
+        enunciado: "Não satisfeito o meliante, te pergunta qual a fórmula de bhaskara, voce:",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Faz algumas pesquisas na internet e assiste vídeo-aulas sobre o assunto.",
+            {
+                texto: "Finge que nem escutou",
+                afirmacao: " ele rouba seu celular"
+            },
+            {
+                texto: "é -b módulo de delta sobre 2a",
+                afirmacao: " ele te da uma paçoca"
+            }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta a educção no futuro. Nesse debate, como você se posiciona?",
+        enunciado: "ainda não satisfeito, o assaltante lhe pergunta o que é um triangulo pitagórico",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas e melhores novam formas de gestão educacional e melhorar o raciocíonio humano.",
-            "Me preocupo com o futuro educacional tangente à educação futura."
+            {
+                texto: " é um triângulo com catetos e hipotenusa em unidades consecutivas ex: (3,4,5)",
+                afirmacao: "o assaltante entende seu erro"
+            },
+            {
+                texto: "é um triângulo com unidades inteiras.",
+                afirmacao: "o assaltante te dá uma bala"
+            }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "o Ladrão fala lhe que fará mais 2 perguntas, ele te pergunta qual o teorema de pitágoras",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
+            {
+                texto: "A soma do quadrado dos catetos é igual ao quadrado da hipotenusa",
+                afirmacao: "ele fica surpreso e pensa em lhe deixar ir"
+            },
+            {
+                texto: "sim!!",
+                afirmacao: "Ele sabe que voce ficou de recuperação em matemática"
+            }
         ]
     },
     {
-        enunciado: "Você precisa fazer o trabalho de matemática de binômios de Newton pedido semana passada e entregá-lo na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        enunciado: "ele prossegue e lhe faz a última pergunta, o que é um triângulo equilatero ",
         alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-        ]
-    },
+            {
+                texto: "não sei",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "é um triângulo que possui todos os lados e ângulos de mesma medida",
+                afirmacao: "Ele te libera e dá uma caixa de paçoca"
+            ]
+        }
 ];
+
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
